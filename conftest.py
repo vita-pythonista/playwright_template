b0ac071ecd1b282b8a8deae36fccc3d163f9d7ed
Vitaly <joyniter@gmail.com>
2023-01-20 15:10:12 +0300

    Fix browser_context_args and README.md

--- conftest.py
+++ conftest.py
@@ -1,6 +1,24 @@
 import allure
 import pytest
 
+@pytest.fixture(scope="session")
+def browser_context_args(request, browser_context_args, playwright):
+    """Запуск с эмуляцией девайсов и без"""
+    device = request.config.getoption("--device")
+    if device:
+        mdevice = playwright.devices[device]
+        return {
+            **browser_context_args,
+            **mdevice,
+        }
+    else:
+        return {
+            **browser_context_args,
+            "viewport": {
+                "width": 1920,
+                "height": 1080,
+            }
+        }
 
 @pytest.fixture(autouse=True)
 def screenshot_on_failure(request, page) -> None:
